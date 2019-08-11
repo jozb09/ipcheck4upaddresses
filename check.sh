ba8093287c74a39478b2e9383324348036cadbdb
jozb09 <jozb09@yahoo.com>
2019-08-11 13:43:02 +0300

    Create check.sh

---
+++ check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ "$1" == "" ]; then
+    echo "You forgot to put the IP address"
+    echo "This is the sytanx: './check.sh 0.0.0' 0.0.0 is the network address"
+else
+    for ip in `seq 1 254`; do
+        ping -c 1 $1.$ip | grep "64 bytes" | cut -d " " -f 4 | tr -d ":" &
+    done
+fi
